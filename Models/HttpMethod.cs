5c4303320b20555ada30e479e8d9ec6ad8204a01
Sebastian Gonzalo Alvarez Concepcion <sebastianalvarezconcepcion@gmail.com>
2026-05-12 22:59:57 -0400

    Add: Models

---
+++ Models/HttpMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiNMIDLE.Models
+{
+    public enum HttpMethod
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+        PATCH,
+        HEAD,
+        OPTIONS,
+        TRACE,
+        CONNECT
+    }
+}
---
+++ Models/Table.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiNMIDLE.Models
+{
+    public class Table
+    {
+        
+        int Id { get; set; }
+        public string Host { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        public HttpMethod Method { get; set; }
+        public string Query { get; set; } = string.Empty;
+        public int StatusCode { get; set; }
+        public string Extension { get; set;} = string.Empty;
+        public int Length { get; set; } = 0;
+        public int Time { get; set; } = 0;
+        public DateTime Date { get; set;  } = DateTime.Now;
+    }
+}
